c561909a5d1427a456f49f6630f78ea4a35396c9
ShaliniShrivastava <sha.shri21@gmail.com>
2026-07-01 17:41:26 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -1,33 +1,34 @@
-const env = require('dotenv');
+const env = require("dotenv");
 env.config();
 
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./database/connectDB');
-const web = require('./routes/web');
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./database/connectDB");
+const web = require("./routes/web");
 const app = express();
-const path = require('path');
+const path = require("path");
 const port = 3000;
 
-
-
-
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use(express.json());        
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use(express.json());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://consultancy-services-website.vercel.app",
+    ],
+    credentials: true,
+  }),
+);
 
 app.use("/api", web);
 
 connectDB();
 
-app.get('/', (req, res) => {
+(app.get("/", (req, res) => {
   res.send({
     activeStatus: true,
     error: false,
-  })
+  });
 }),
-
-module.exports = app
+  (module.exports = app));
